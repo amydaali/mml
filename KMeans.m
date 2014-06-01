6e2df49219d7c0912b934fdcda3b38b195a2d145
MartinHjelm <mar.hjelm@gmail.com>
2014-06-02 00:04:14 +0800

    Changed to row vectors instead of..

--- KMeans.m
+++ KMeans.m
@@ -1,4 +1,4 @@
-function [Mu, r] = KMeans ( X, k, max_iter, varargin ) 
+function [Mu, r, ivs] = KMeans ( X, k, max_iter, varargin ) 
 % function [Mu, r] = KMeans ( X, k, max_iter, prog_fun ) 
 % Kmeans - K-means Clustering Algorithm
 %
@@ -6,7 +6,7 @@ function [Mu, r] = KMeans ( X, k, max_iter, varargin )
 %     [Mu, r] = KMeans ( X, k, max_iter, prog_fun ) 
 %
 % input
-%        X : (m,n)-matrix of column vectors
+%        X : (m,n)-matrix of row vectors
 %        k : number of cluster centers
 %        max_iter : maximum number of iterations
 %        prog_fun : I dunno....  
@@ -26,11 +26,6 @@ function [Mu, r] = KMeans ( X, k, max_iter, varargin )
     error('KMeans.m: Too few input arguments. For help type help KMeans.\n');    
   end
 
-  % Check if dist2 function is present. 
-  if ~exist('dist2')
-    error('KMeans requires dist2 function by Ian Nabey')
-  end
-
   % Set default values for the number of iterations, if not set.
   if ~(exist('max_iter','var'))
     max_iter = 100;
@@ -45,24 +40,29 @@ function [Mu, r] = KMeans ( X, k, max_iter, varargin )
   
 
   %  1. Choose k random cluster centers and iniate storage matrices
-  [~, N] = size ( X );
+  [N_data,~] = size ( X );
   if ~clusterCentersSet
-    Mu = X ( :, randi ( N, k, 1, 'uint32' ) ); % cluster  matrix
+    Mu = X ( randi ( N_data, k, 1, 'uint32' ), :); % cluster  matrix
   end
   % vector
-  r  = zeros ( N, 1 );  % index vector  
+  r  = zeros ( N_data, 1 );  % index vector  
 
   %  2. Do iteration up till max_iter or till no change is observable
   for i_iter = 1:max_iter
     %disp(['Iteration ',num2str(i),' of ',num2str(max_iter)]); drawnow;
 
     % Find new nearest cluster center for each point
-    [~,r_new] = min(dist2(X', Mu'),[],2);
+    [~,r_new] = min(pdist2(X, Mu),[],2);
     
     % Compute new cluster centre for each by taking the mean of every point from
     % given the new assigments.
     for j = 1:k
-      Mu(:,j) = mean ( X(:, ( r_new == j ) ), 2 );     
+      clusterIdxs = r_new == j;
+      if(sum(clusterIdxs))
+        Mu(j,:) = mean( X(clusterIdxs,:) );     
+      else
+        Mu(j,:) = X ( randi ( N_data, 1, 1, 'uint32' ), :);
+      end
     end
     
     % Check if cluster assignment has changed, break if not.
@@ -81,6 +81,17 @@ function [Mu, r] = KMeans ( X, k, max_iter, varargin )
 %       feval ( prog_fun, X, Mu, r );
 %       pause 
 %     end
+
+    ivs = 0;
+    for i_center = 1:k
+      N_cps = sum(r == i_center);
+      Xcps = X(( r == i_center ),:);
+      XcpsBar = Mu(i_center,:);
+      ivs = ivs + sumall(Xcps - (ones(N_cps,1)*XcpsBar).^2);
+    end
+    ivs = sum(ivs);
+
+
 
   end
   
